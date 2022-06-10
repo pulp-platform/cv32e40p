800a09d97a1e9418e127e8bbf1763c1d1097c92f
Robert Balas <balasr@iis.ee.ethz.ch>
2022-06-10 20:03:10 +0200

    rtl/include: Add one pipeline stage to fpu and divsqrt

--- rtl/include/cv32e40p_pkg.sv
+++ rtl/include/cv32e40p_pkg.sv
@@ -735,11 +735,11 @@ package cv32e40p_pkg;
 
   // Latency of FP operations: 0 = no pipe registers, 1 = 1 pipe register etc.
   parameter int unsigned C_LAT_FP64    = 'd0;
-  parameter int unsigned C_LAT_FP32    = 'd0;
+  parameter int unsigned C_LAT_FP32    = 'd1;
   parameter int unsigned C_LAT_FP16    = 'd0;
   parameter int unsigned C_LAT_FP16ALT = 'd0;
   parameter int unsigned C_LAT_FP8     = 'd0;
-  parameter int unsigned C_LAT_DIVSQRT = 'd1;  // divsqrt post-processing pipe
+  parameter int unsigned C_LAT_DIVSQRT = 'd2;  // divsqrt post-processing pipe
   parameter int unsigned C_LAT_CONV    = 'd0;
   parameter int unsigned C_LAT_NONCOMP = 'd0;
 
